c32923938fc26b8ad6db26df92ff361d15d19c86
Anatoly Kanivetskiy <chaos.a.d88@gmail.com>
2013-03-07 11:20:53 +0100

    get/set/del/exists functions were implemented

--- src/storage.erl
+++ src/storage.erl
@@ -2,6 +2,11 @@
 
 -export([start/0, stop/0]).
 
+-export([get/1, get/3]).
+-export([del/1, del/3]).
+-export([set/2, set/4]).
+-export([exists/1, exists/3]).
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 start() ->
@@ -29,3 +34,57 @@ start_ok(App, {error, Reason}) ->
     erlang:error({app_start_failed, App, Reason}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+exists(Key) ->
+    case sharded_eredis:q(["EXISTS", str(Key)]) of
+        {ok, <<"1">>} -> true;
+        {ok, <<"0">>} -> false;
+        {error, Error} -> error(Error)
+    end.
+
+exists(GameID, UserID, Key) ->
+    exists(key(GameID, UserID, Key)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+get(Key) ->
+    case sharded_eredis:q(["GET", str(Key)]) of
+        {ok, Result} -> Result;
+        {error, Error} -> error(Error)
+    end.
+
+get(GameID, UserID, Key) ->
+    ?MODULE:get(key(GameID, UserID, Key)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+set(Key, Value) ->
+    case sharded_eredis:q(["SET", str(Key), str(Value)]) of
+        {ok, <<"OK">>} -> ok;
+        {error, Error} -> error(Error)
+    end.
+
+set(GameID, UserID, Key, Value) ->
+    ?MODULE:set(key(GameID, UserID, Key), Value).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+del(Key) ->
+    case sharded_eredis:q(["DEL", str(Key)]) of
+        {ok, Result} -> list_to_integer(binary_to_list(Result));
+        {error, Error} -> error(Error)
+    end.
+
+del(GameID, UserID, Key) ->
+    ?MODULE:del(key(GameID, UserID, Key)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Internal functions:
+
+key(GameID, UserID, Key) ->
+    string:join([str(GameID), str(UserID), str(Key)], ".").
+
+str(Value) when is_list(Value) -> Value;
+str(Value) when is_atom(Value) -> atom_to_list(Value);
+str(Value) when is_binary(Value) -> binary_to_list(Value);
+str(Value) when is_integer(Value) -> integer_to_list(Value).
